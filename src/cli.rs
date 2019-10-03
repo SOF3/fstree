498be8d953c29fd21ae1acc54149e3e8ce76ce27
SOFe <sofe2038@gmail.com>
2019-10-03 14:02:38 +0800

    Conditional features

--- src/cli.rs
+++ src/cli.rs
@@ -27,7 +27,7 @@ pub struct CommandArgs {
 }
 
 pub fn read() -> Result<CommandArgs> {
-    let app = clap::App::new(env!("CARGO_PKG_NAME"))
+    let mut app = clap::App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -38,15 +38,45 @@ pub fn read() -> Result<CommandArgs> {
                 .index(1),
         )
         .arg(clap::Arg::with_name("shake")
-             .help("Sets the upper bound for leaf shaking. Nodes with size below this value will not be stored.")
+             .help("Sets the upper bound for leaf shaking. Files below this size will be blackboxed in storage and visualization (but still contribute to total size)")
              .default_value("1MiB")
-             .long("shake"))
-        .get_matches();
+             .long("shake"));
+    #[cfg(feature = "web")]
+    {
+        app = app
+            .arg(
+                clap::Arg::with_name("no-web")
+                    .help("Do not start web frontend to view the report")
+                    .long("no-web"),
+            )
+            .arg(
+                clap::Arg::with_name("port")
+                    .help("Sets the port to start web frontend on")
+                    .default_value("8000")
+                    .short("p")
+                    .long("port"),
+            );
+    }
+    #[cfg(feature = "history")]
+    {
+        app = app.arg(clap::Arg::with_name("no-write")
+            .help("Do not write the results to history")
+            .long("no-write"))
+            .arg(clap::Arg::with_name("history-path")
+                 .help("Sets the directory that history is stored in")
+                 .default_value("~/.fstree/history")
+                 .long("history-path"))
+            .arg(clap::Arg::with_name("rotate")
+                 .help("When adding a history file, older history files older than this number of days will be discarded")
+                 .default_value("30")
+                 .long("rotate"));
+    }
+    let args = app.get_matches();
 
-    let dir = app.value_of("dir").unwrap();
+    let dir = args.value_of("dir").unwrap();
     let dir = PathBuf::from(dir);
 
-    let shake = app.value_of("shake").unwrap();
+    let shake = args.value_of("shake").unwrap();
     let shake = Byte::from_str(shake).map_err(|_| make_err("Failed to parse --shake"))?;
 
     Ok(CommandArgs { dir, shake })
